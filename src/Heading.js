d6e073f7ae3a970d5250df2dd64ae4fc764802e4
Max Veregge <maxwell.veregge@gmail.com>
2020-04-27 19:37:15 -0400

    fix styling of heading

--- src/Heading.js
+++ src/Heading.js
@@ -7,7 +7,7 @@ function Heading() {
         {" "}
         Overtime Randomizer{" "}
       </h1>
-      <div class="row heading-container">
+      <div class="heading-container">
         <h3 class="col-lg-4">Mechanism</h3>
         <h3 class="col-lg-4">Time Period</h3>
         <h3 class="col-lg-4">Basis</h3>
